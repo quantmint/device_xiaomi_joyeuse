b51c42ac8c95d1ea0022bab0396e59ef2acedcb9
Lunatic Walker <59589591+skylark17@users.noreply.github.com>
2020-07-28 16:22:47 +0700

    joyeuse : msm-ify

--- xtended_joyeuse.mk
+++ xtended_joyeuse.mk
@@ -7,10 +7,13 @@
 $(call inherit-product, device/xiaomi/joyeuse/device.mk)
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_curtana
+PRODUCT_NAME := xtended_joyeuse
 PRODUCT_DEVICE := joyeuse
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 9 Pro
 PRODUCT_MANUFACTURER := Redmi
+
+TARGET_BOOT_ANIMATION_RES := 1080
+SKIP_ABI_CHECKS=true
 
 BUILD_FINGERPRINT := "Redmi/curtana_global/curtana:10/QKQ1.191215.002/V11.0.9.0.QJWMIXM:user/release-keys"
