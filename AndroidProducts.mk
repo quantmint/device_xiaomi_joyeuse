936a0e088ccd09ec61dbcb35b1ea723eb2be965b
QuantMint <quantmint@protonmail.com>
2020-08-08 00:13:23 +0200

    Arrow bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_joyeuse.mk
+    $(LOCAL_DIR)/arrow_joyeuse.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_joyeuse-user \
-    lineage_joyeuse-userdebug \
-    lineage_joyeuse-eng
+    arrow_joyeuse-user \
+    arrow_joyeuse-userdebug \
+    arrow_joyeuse-eng
